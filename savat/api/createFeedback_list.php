5fc0f3696370142e3a0089b6cc965f7f57a58bca
Mekhriniso <mkenjayeva784@gmail.com>
2020-05-10 17:08:51 +0500

    api

---
+++ savat/api/createFeedback_list.php
@@ -0,0 +1,36 @@
+<?php
+require 'database.php';
+
+// Get the posted data.
+$postdata = file_get_contents("php://input");
+
+if(isset($postdata) && !empty($postdata))
+{
+  // Extract the data.
+  $request = json_decode($postdata);
+
+
+  // Sanitize.
+  $customer_id = mysqli_real_escape_string($con, (int)$request->customer_id);
+  $email = mysqli_real_escape_string($con, trim($request->email));
+  $comments = mysqli_real_escape_string($con, trim($request->comments));
+    
+
+  // Create.
+  $sql = "INSERT INTO 'feedback_list'('customer_id', 'email', 'comments') VALUES ('{$customer_id}','{$email}', null)";
+
+  if(mysqli_query($con,$sql))
+  {
+    http_response_code(201);
+    $feedback_list = [
+      'customer_id' => $customer_id,
+      'email' => $email,
+      'comments' => mysqli_insert_created_at($con)
+    ];
+    echo json_encode($feedback_list);
+  }
+  else
+  {
+    http_response_code(422);
+  }
+}
