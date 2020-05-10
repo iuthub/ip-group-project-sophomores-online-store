b914cca8fc120c509313eb653fb1e50694e4b2cc
Mekhriniso <mkenjayeva784@gmail.com>
2020-05-10 17:29:17 +0500

    Update readfeedback_list.php

--- savat/api/readfeedback_list.php
+++ savat/api/readfeedback_list.php
@@ -3,15 +3,14 @@
 require 'database.php';
 
 $feedback_list = [];
-$sql = "SELECT costumer_id, email, comments  FROM feedback_list";
+$sql = "SELECT email, comments  FROM feedback_list";
 
 if($result = mysqli_query($con,$sql))
 {
   $i = 0;
   while($row = mysqli_fetch_assoc($result))
   {
-   
-    $feedback_list[$i]['costumer_id'] = $row[' '];
+  
     $feedback_list[$i]['email'] = $row['email'];
     $feedback_list[$i]['comments'] = $row['comments'];
     $i++;
