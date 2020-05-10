0478da29e622dc9a21b2c67561dea45699273801
Saidazim <saidazimabduhamidov@gmail.com>
2020-05-10 20:43:31 +0500

    finishing project

--- savat/angular/src/app/components/login/sign-up/sign-up.component.ts
+++ savat/angular/src/app/components/login/sign-up/sign-up.component.ts
@@ -9,15 +9,30 @@ import { Customers } from 'src/app/models/customers';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  hide = true;
+  registration = new FormGroup({});
+  customers: Customers = new Customers();
 
+  constructor(private customerService: CustomerService,
+              private fb: FormBuilder) {
 
-  constructor() {
-
-
+    this.registration = fb.group({
+      'login': ['', [Validators.min(5), Validators.max(16)]],
+      'password': ['', [Validators.min(5), Validators.max(16)]],
+      'email': ['', [Validators.email]],
+      'phone_number': ['', [Validators.pattern('[6-9]\\d{11}')]],
+      'postal_code': ['', [Validators.pattern('[0-9]{7}')]],
+      'passport_number': ['', [Validators.pattern('[A-Z]{2}[0-9]{7}')]]
+    })
   }
 
   ngOnInit() {
   }
 
-
+  signUp() {
+    this.customerService.register(this.customers)
+      .subscribe(data => console.log(data),
+        error => console.log(error));
+    this.customers = new Customers();
+  }
 }
