a49b7aa92d01138fc22de9ef48aea917ae420f35
Saidazim <saidazimabduhamidov@gmail.com>
2020-05-10 20:50:41 +0500

    finishing project

--- savat/database/seeds/DatabaseSeeder.php
+++ savat/database/seeds/DatabaseSeeder.php
@@ -16,22 +16,3 @@ class DatabaseSeeder extends Seeder
        $this->call(OrderItemsTableSeeder::class);
     }
 }
-=======
-<?php
-
-use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-       $this->call(OrdersTableSeeder::class);
-       $this->call(OrderItemsTableSeeder::class);
-    }
-}
->>>>>>> b914cca8fc120c509313eb653fb1e50694e4b2cc
