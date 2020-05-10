41b8aadbc805b7c87ee1d52a22e7991e6de8f5e4
Saidazim <saidazimabduhamidov@gmail.com>
2020-05-10 18:52:25 +0500

    solving conflicts

--- savat/angular/src/app/models/products.ts
+++ savat/angular/src/app/models/products.ts
@@ -1,14 +1,5 @@
 export class Products {
-<<<<<<< HEAD
   product_id: number;
   name: string;
   price: number;
 }
-=======
-  id: number;
-  name: string;
-  price: number;
-  units: string;
-  quantity: string;
-}
->>>>>>> b914cca8fc120c509313eb653fb1e50694e4b2cc
